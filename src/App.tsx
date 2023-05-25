07fc4762116d53b46042bc88f679df51db8eafaf
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-05-25 12:30:23 +0100

    typing an object prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css';
 import { Greet } from './components/Greet';
+import { Person } from './components/Person';
 
 function App() {
+  const personName = {
+    first: 'Tony',
+    last: 'Stark',
+  };
   return (
     <>
       <Greet name="Pope" messageCount={28} isLoggedIn={false} />
+      <Person name={personName} />
     </>
   );
 }
