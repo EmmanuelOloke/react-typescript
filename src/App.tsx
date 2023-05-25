434c19d19502e31794e14f819298148c1eb9dd85
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-05-25 12:49:26 +0100

    working with array of objects prop types

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
 import './App.css';
 import { Greet } from './components/Greet';
 import { Person } from './components/Person';
+import { PersonList } from './components/PersonList';
 
 function App() {
   const personName = {
     first: 'Tony',
     last: 'Stark',
   };
+
+  const nameList = [
+    {
+      first: 'Captain',
+      last: 'America',
+    },
+    {
+      first: 'Scarlet',
+      last: 'Witch',
+    },
+    {
+      first: 'Black',
+      last: 'Widow',
+    },
+  ];
+
   return (
     <>
       <Greet name="Pope" messageCount={28} isLoggedIn={false} />
       <Person name={personName} />
+      <PersonList names={nameList} />
     </>
   );
 }
