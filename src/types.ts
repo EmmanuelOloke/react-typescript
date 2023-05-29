f2f3a27800aaa54d9aca1f62aa3d970cd13c9459
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-05-30 00:29:10 +0100

    moved in type definitions for Counter component useReducer

--- src/types.ts
+++ src/types.ts
@@ -38,3 +38,18 @@ export type AuthUser = {
   name: string;
   email: string;
 };
+
+export type CounterState = {
+  count: number;
+};
+
+export type UpdateAction = {
+  type: 'increment' | 'decrement';
+  payload: number;
+};
+
+export type ResetAction = {
+  type: 'reset';
+};
+
+export type CounterAction = UpdateAction | ResetAction;
