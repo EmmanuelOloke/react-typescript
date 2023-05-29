5a20b3a1878191b15867d4e49a6eeb52a93bd8d8
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-05-29 03:33:29 +0100

    typing useState hook with future value

---
+++ src/components/state/User.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import { AuthUser } from '../../types';
+
+export const User = () => {
+  const [user, setUser] = useState<AuthUser | null>(null);
+  const handleLogin = () => {
+    setUser({
+      name: 'Emmanuel',
+      email: 'emmanuel@example.com',
+    });
+  };
+
+  const handleLogout = () => {
+    setUser(null);
+  };
+  return (
+    <div>
+      <button onClick={handleLogin}>Login</button>
+      <button onClick={handleLogout}>Logout</button>
+      <div>User name is {user?.name}</div>
+      <div>User email is {user?.email}</div>
+    </div>
+  );
+};
