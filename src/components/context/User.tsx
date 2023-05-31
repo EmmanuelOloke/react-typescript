25b874fb23ee8f1032be088847737a6eb36dcdaa
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-05-31 23:14:17 +0100

    User component for context

---
+++ src/components/context/User.tsx
@@ -0,0 +1,15 @@
+export const User = () => {
+  const handleLogin = () => {
+    // some code here
+  };
+  const handleLogout = () => {
+    // some code here
+  };
+  return (
+    <div>
+      <button onClick={handleLogin}></button>
+      <button onClick={handleLogout}></button>
+      <div>User name is</div>
+    </div>
+  );
+};
