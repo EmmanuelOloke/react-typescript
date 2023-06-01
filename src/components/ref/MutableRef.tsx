624abccbae79acf6a8951a8ec5a0a97a49dcafc3
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-06-01 15:45:58 +0100

    setting the type of intervalRef

--- src/components/ref/MutableRef.tsx
+++ src/components/ref/MutableRef.tsx
@@ -2,7 +2,7 @@ import { useState, useRef, useEffect } from 'react';
 
 export const MutableRef = () => {
   const [timer, setTimer] = useState(0);
-  const intervalRef = useRef(null);
+  const intervalRef = useRef<number | null>(null);
 
   const stopTimer = () => {
     window.clearInterval(intervalRef.current);
