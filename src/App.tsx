26052b55bffdfdab6a5bd1df2f9ceb203e80e80e
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-06-03 00:37:38 +0100

    rendering the polymorphic component

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ import { List } from './components/generics/List';
 import { RandomNumber } from './components/restriction/RandomNumber';
 import { Toast } from './components/templateliterals/Toast';
 import { CustomButton } from './components/html/Button';
+import { Text } from './components/polymorphic/Text';
 
 function App() {
   const personName = {
@@ -105,6 +106,16 @@ function App() {
       <CustomButton variant="primary" onClick={() => console.log('Clicked')}>
         Primary Button
       </CustomButton>
+
+      <Text as="h1" size="lg">
+        Heading
+      </Text>
+      <Text as="p" size="md">
+        Paragraph
+      </Text>
+      <Text as="label" htmlFor="someId" size="sm" color="secondary">
+        Label
+      </Text>
     </>
   );
 }
