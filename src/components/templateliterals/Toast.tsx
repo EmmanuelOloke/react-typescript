00727bdcee814af78f70a8f418dbe107068a42bf
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-06-02 23:45:19 +0100

    excluding center-center prop and allowing only center prop

--- src/components/templateliterals/Toast.tsx
+++ src/components/templateliterals/Toast.tsx
@@ -8,7 +8,7 @@ type HorizontalPosition = 'left' | 'center' | 'right';
 type VerticalPosition = 'top' | 'center' | 'bottom';
 
 type ToastProps = {
-  position: `${HorizontalPosition}-${VerticalPosition}`;
+  position: Exclude<`${HorizontalPosition}-${VerticalPosition}`, 'center-center'> | 'center';
 };
 
 export const Toast = ({ position }: ToastProps) => {
