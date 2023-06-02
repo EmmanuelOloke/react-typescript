c478ba3132dba44d3b101143f7af58024b18cba7
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-06-03 00:01:23 +0100

    testing custom button element

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ import { Profile } from './components/auth/Profile';
 import { List } from './components/generics/List';
 import { RandomNumber } from './components/restriction/RandomNumber';
 import { Toast } from './components/templateliterals/Toast';
+import { CustomButton } from './components/html/Button';
 
 function App() {
   const personName = {
@@ -100,6 +101,10 @@ function App() {
       <RandomNumber value={5} isPositive />
 
       <Toast position="center" />
+
+      <CustomButton variant="primary" onClick={() => console.log('Clicked')}>
+        Primary Button
+      </CustomButton>
     </>
   );
 }
